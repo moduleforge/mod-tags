615534cd52feecc37888a5f558e56773c5ca4c8c
Zane Rockenbaugh <zane@liquid-labs.com>
2026-07-10 17:52:40 -0500

    wip(mod-tags-migrate-package): add migrate.go + goose dependency; module builds

---
+++ model/migrations/migrate.go
@@ -0,0 +1,24 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"embed"
+
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed *.sql
+var FS embed.FS
+
+// TableName is this module's dedicated goose version-tracking table.
+const TableName = "goose_db_version_tags"
+
+func Migrate(ctx context.Context, db *sql.DB) error {
+	goose.SetBaseFS(FS)
+	if err := goose.SetDialect("postgres"); err != nil {
+		return err
+	}
+	goose.SetTableName(TableName)
+	return goose.UpContext(ctx, db, ".")
+}
